a06e1ce89fe323345579c307bf2f0b7d4ad0153f
Tyrael Tong <Tyrael.Tong@activenetwork.com>
2012-01-26 11:08:28 +0800

    remove all the empty or skeleton spec files.

--- marquee-web-main/spec/models/suite_selection_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe SuiteSelection do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
--- marquee-web-main/spec/models/test_plan_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe TestPlan do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
