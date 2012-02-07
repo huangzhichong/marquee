ca32db1387bd53128104288490ff87c5f624859a
Leo Yin <Leo.Yin@activenetwork.com>
2012-02-08 01:07:35 +0800

    Fix a bug which may cause JSON generating error. Add time information to SimplifiedSlaveAssignment definition

--- marquee-web-main/app/helpers/slave_assignments_helper.rb
+++ marquee-web-main/app/helpers/slave_assignments_helper.rb
@@ -2,31 +2,24 @@ module SlaveAssignmentsHelper
 
   def SlaveAssignmentsHelper.send_slave_assignment_to_list(slave_assignment, list_name)
 
-    puts "Inserting into #{list_name} list: #{slave_assignment.inspect}"
-
     slave_assignments = $redis.hgetall :slave_assignments
 
     simplifiedSA = SimplifiedSlaveAssignment.new(slave_assignment)
-    puts "simplifiedSA: #{simplifiedSA.inspect}"
 
     # if slave assignments is not nil and expected list has been created, update it
     slave_assignments.each do |name, value|
       sa_list = JSON.parse value
-      puts "#{name} list: #{sa_list.inspect}"
       # remove it if it exist
       sa_list.reject! {|sa| sa.nil? || sa["id"] == simplifiedSA.id}
       # add to list if the list is what we expected to add in
-      sa_list << simplifiedSA if name == list_name.to_s
-      puts "Updated #{name} list: #{sa_list.inspect}"
+      sa_list << JSON.parse(simplifiedSA.to_json) if name == list_name.to_s
       $redis.hset :slave_assignments, name, JSON.generate(sa_list)
-      puts "Fetched #{name} list: #{$redis.hget :slave_assignments, name}"
     end 
 
     # if slave assignments is nil or expected list has't been created, create it
     if slave_assignments.nil? || !slave_assignments.has_key?(list_name.to_s)
       sa_list = Array.new
-      sa_list << simplifiedSA
-      puts "Newly created #{list_name} list: #{sa_list.inspect}"
+      sa_list << JSON.parse(simplifiedSA.to_json)
       $redis.hset :slave_assignments, list_name, JSON.generate(sa_list)
     end
 
@@ -38,9 +31,15 @@ class SimplifiedSlaveAssignment
 
   attr_accessor :id
   attr_accessor :slave_id
+  attr_accessor :time_out_limit
+  attr_accessor :created_at
+  attr_accessor :updated_at
 
   def initialize(slave_assignment)
     @id = slave_assignment.id
     @slave_id = slave_assignment.slave_id
+    @time_out_limit = slave_assignment.automation_script_result.automation_script.time_out_limit
+    @created_at = slave_assignment.created_at
+    @updated_at = slave_assignment.updated_at
   end
 end 
