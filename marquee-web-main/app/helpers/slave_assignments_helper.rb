f67e0cf9d667bb364d544b94116cfb2733391c11
Tyrael Tong <Tyrael.Tong@activenetwork.com>
2012-02-21 23:27:29 +0800

    remove unused simplified slave assignment class

--- marquee-web-main/app/helpers/slave_assignments_helper.rb
+++ marquee-web-main/app/helpers/slave_assignments_helper.rb
@@ -4,8 +4,6 @@ module SlaveAssignmentsHelper
 
     slave_assignments = $redis.hgetall :slave_assignments
 
-    #simplifiedSA = SimplifiedSlaveAssignment.new(slave_assignment)
-
     # if slave assignments is not nil and expected list has been created, update it
     slave_assignments.each do |name, value|
       sa_list = JSON.parse value
@@ -18,29 +16,9 @@ module SlaveAssignmentsHelper
 
     # if slave assignments is nil or expected list has't been created, create it
     if slave_assignments.nil? || !slave_assignments.has_key?(list_name.to_s)
-      sa_list = Array.new
-#      sa_list << JSON.parse(simplifiedSA.to_json)
-      sa_list << slave_assignment.as_json
-      $redis.hset :slave_assignments, list_name, JSON.generate(sa_list)
+      $redis.hset :slave_assignments, list_name, JSON.generate([slave_assignment.as_json])
     end
 
   end
 
-end
-
-class SimplifiedSlaveAssignment
-
-  attr_accessor :id
-  attr_accessor :slave_id
-  attr_accessor :time_out_limit
-  attr_accessor :created_at
-  attr_accessor :updated_at
-
-  def initialize(slave_assignment)
-    @id = slave_assignment.id
-    @slave_id = slave_assignment.slave_id
-    @time_out_limit = slave_assignment.automation_script_result.automation_script.time_out_limit
-    @created_at = slave_assignment.created_at
-    @updated_at = slave_assignment.updated_at
-  end
 end
