4311eda11a1733e692be9c2dfabfd68f7712846b
Leo Yin <Leo.Yin@activenetwork.com>
2012-02-28 17:38:50 +0800

    ability definitions could be assigned to roles in Admin/Role module now

--- marquee-web-main/app/models/ability_definition.rb
+++ marquee-web-main/app/models/ability_definition.rb
@@ -1,6 +1,7 @@
 class AbilityDefinition < ActiveRecord::Base
   # belongs_to :role
   has_and_belongs_to_many :roles
+  has_and_belongs_to_many :users
   
   acts_as_audited :protect => false
 end
--- marquee-web-main/app/models/role.rb
+++ marquee-web-main/app/models/role.rb
@@ -10,5 +10,7 @@ class Role < ActiveRecord::Base
   # has_and_belongs_to_many :users
   has_and_belongs_to_many :ability_definitions
 
+  validates :name, :presence => true, :uniqueness => true
+
   acts_as_audited :protect => false
 end
