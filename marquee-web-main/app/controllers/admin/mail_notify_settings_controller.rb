eed01845c7a037f52250d10963b76bf345371135
Tyrael Tong <Tyrael.Tong@activenetwork.com>
2012-03-07 13:19:49 +0800

    fix the bug

--- marquee-web-main/app/controllers/admin/mail_notify_settings_controller.rb
+++ marquee-web-main/app/controllers/admin/mail_notify_settings_controller.rb
@@ -13,6 +13,10 @@ class Admin::MailNotifySettingsController < InheritedResources::Base
       render :action => "new" and return
     end
 
-    super
+    create!{admin_project_mail_notify_settings_url(project)}
+  end
+
+  def update
+    update!{admin_project_mail_notify_settings_url(@project)}
   end
 end
