77a5f61ad29bd5984b4b9bf6dd976352e240e19d
Brent Huang <Brent.Huang@activenetwork.com>
2012-03-15 15:22:26 +0800

    Fix color

--- marquee-web-main/app/models/report/project_severity_bugs.rb
+++ marquee-web-main/app/models/report/project_severity_bugs.rb
@@ -3,16 +3,24 @@ class Report::ProjectSeverityBugs
 
 	def initialize(sev1_bugs, sev2_bugs)
 		@sev1_bugs = sev1_bugs
-		@sev1_bugs_color = severity_color(sev1_bugs)
+		@sev1_bugs_color = severity_sev1_color(sev1_bugs)
 		@sev2_bugs = sev2_bugs
-		@sev2_bugs_color = severity_color(sev2_bugs)
+		@sev2_bugs_color = severity_sev2_color(sev2_bugs)
 	end
 
-	def severity_color(count)
-		if count > 70
+	def severity_sev1_color(count)
+		if count > 0
 			return "red"
-		elsif count > 40
+		else
+			return "green"
+		end
+	end
+
+	def severity_sev2_color(count)
+		if count >= 1 and count <=5
 			return "yellow"
+		elsif count > 5
+			return "red"
 		else
 			return "green"
 		end
