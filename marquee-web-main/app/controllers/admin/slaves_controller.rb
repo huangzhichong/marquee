ae79720086a93e61a6fcbdf7d7d30f26faeff005
Leo Yin <Leo.Yin@activenetwork.com>
2012-03-22 17:18:54 +0800

    MQEE-239 redefine singular format of Slaves from Slafe to Slave

--- marquee-web-main/app/controllers/admin/slaves_controller.rb
+++ marquee-web-main/app/controllers/admin/slaves_controller.rb
@@ -2,57 +2,16 @@ class Admin::SlavesController < InheritedResources::Base
 
   layout 'admin'
   before_filter :authenticate_user!
-  load_and_authorize_resource :class => :Slave
+  load_and_authorize_resource
 
   def create
 
-    save_slave(@slave, params)
-
-    if @slave.errors.any?
-      render :new and return
-    else
-      redirect_to admin_slaves_url
-    end
+    create!{admin_slaves_url}
   end
 
   def update
 
-    save_slave(@slave, params)
-
-    if @slave.errors.any?
-      p @slave.errors
-      render :edit and return
-    else
-      redirect_to admin_slaves_url
-    end
-  end
-
-  protected
-
-  def collection
-    @slaves = Slave.all
-  end
-
-  def resource
-    @slave ||= Slave.find(params[:id])
-  end
-
-  def build_resource
-    @slave ||= Slave.new
-  end
-
-  private
-
-  def save_slave(slave, params)
-
-    slave.name = params[:slave][:name]
-    slave.ip_address = params[:slave][:ip_address]
-    slave.project_name = params[:slave][:project_name]
-    slave.status = params[:slave][:status]
-    slave.test_type = params[:slave][:test_type]
-    slave.priority = params[:slave][:priority]
-
-    slave.save
+    update!{admin_slaves_url}
   end
 
 end
