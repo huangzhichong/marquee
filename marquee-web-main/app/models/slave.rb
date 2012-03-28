f5728523b5e2561ddd1458dc22a80c5d8b2cea72
Leo Yin <Leo.Yin@activenetwork.com>
2012-03-28 17:13:28 +0800

    MQEE-274 remove active validation from Slave model

--- marquee-web-main/app/models/slave.rb
+++ marquee-web-main/app/models/slave.rb
@@ -17,7 +17,7 @@ class Slave < ActiveRecord::Base
   has_many :slave_assignments
   has_many :automation_script_results, :through => :slave_assignments
 
-  validates_presence_of :name, :ip_address, :project_name, :test_type, :priority, :active
+  validates_presence_of :name, :ip_address, :project_name, :test_type, :priority
   validates_uniqueness_of :name, :ip_address, :case_sensitive => false, :message => " already exists."
 
   def free!
