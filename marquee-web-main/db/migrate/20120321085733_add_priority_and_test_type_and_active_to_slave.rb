348a7ab73eed2db41b470d19feb7504dd1356129
Leo Yin <Leo.Yin@activenetwork.com>
2012-03-28 17:47:32 +0800

    MQEE-274 set default test_type to * for old slaves

--- marquee-web-main/db/migrate/20120321085733_add_priority_and_test_type_and_active_to_slave.rb
+++ marquee-web-main/db/migrate/20120321085733_add_priority_and_test_type_and_active_to_slave.rb
@@ -1,6 +1,6 @@
 class AddPriorityAndTestTypeAndActiveToSlave < ActiveRecord::Migration
   def change
-    add_column :slaves, :test_type, :string
+    add_column :slaves, :test_type, :string, :default => "*"
     add_column :slaves, :priority, :integer, :default => 10
     add_column :slaves, :active, :boolean, :default => true
   end
