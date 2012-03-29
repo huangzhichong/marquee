59625fe33a8b279f51e60f054dcb27e19eece970
Leo Yin <Leo.Yin@activenetwork.com>
2012-03-29 17:14:39 +0800

    add pagination to admin->slave module

--- marquee-web-main/app/controllers/admin/slaves_controller.rb
+++ marquee-web-main/app/controllers/admin/slaves_controller.rb
@@ -1,8 +1,15 @@
 class Admin::SlavesController < InheritedResources::Base
 
   layout 'admin'
+  respond_to :js
+
   before_filter :authenticate_user!
   load_and_authorize_resource
+
+  def collection
+    @search = Slave.scoped.search(params[:search])
+    @slaves = @search.page(params[:page]).order('name').per(15)
+  end
 
   def create
 
