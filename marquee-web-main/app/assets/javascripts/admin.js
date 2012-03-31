bc227e52e53eb6b4191d269881758eb30b483fd6
Leo Yin <Leo.Yin@activenetwork.com>
2012-03-31 12:26:18 +0800

    MQEE-280 remove validation function checkOptionSelected and checkOptionItems

--- marquee-web-main/app/assets/javascripts/admin.js
+++ marquee-web-main/app/assets/javascripts/admin.js
@@ -15,21 +15,3 @@ function checkNullSelectBox(field, rules, i, options) {
     return "* Please add at least one item";
   }
 }
-function checkOptionItems(field, rules, i, options){
-  if(field[0].options && field[0].options.length == 0) {
-    return "* This field is required";
-  }
-}
-function checkOptionSelected(field, rules, i, options) {
-  var result = false;
-  if(field[0].options) {
-    for(var i=0; i<field[0].options.length; i++) {
-      if(field[0].options[i].selected == true) {
-        result = true;
-      }
-    }
-  }
-  if(!result) {
-    return "* You must select one item"
-  }
-}
