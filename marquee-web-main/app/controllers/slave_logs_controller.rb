81d7dd84e2dd349bc7c41471a4201606dc695899
Smart Huang <Smart.Huang@activenetwork.com>
2012-09-19 18:09:06 +0800

    finish the slave log view

--- marquee-web-main/app/controllers/slave_logs_controller.rb
+++ marquee-web-main/app/controllers/slave_logs_controller.rb
@@ -2,10 +2,12 @@ class SlaveLogsController < ApplicationController
   layout 'application'
 
   def index    
-    # @search = Log.where( '$and' => [{:ip => params[:ip]},{:timestamp => {'$gt' => Time.zone.parse(params[:timestamp])}}]).sort(:_id.asc)
-    @slave_logs = SlaveLog.all
+    if params[:ip]
+      start_time = params[:start_time] ? Time.zone.parse(params[:start_time].to_s) - 30: Time.now-900
+      end_time = params[:end_time] ? Time.zone.parse(params[:end_time].to_s) + 30 : Time.now 
+      @searched_logs = SlaveLog.where(:ip => params[:ip],:timestamp.gt => start_time,:timestamp.lt => end_time).asc(:_id)
+    end
     respond_to do |format|
-      format.js
       format.html # index.html.erb
       format.json { render json: @logs }
     end
