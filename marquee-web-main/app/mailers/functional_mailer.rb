5850f9cafe52ba222b2dca47b4a82f7e78d9f386
Tyrael Tong <tyrael.tong@activenetwork.com>
2013-02-27 13:14:16 +0800

    update mail address in mailer

--- marquee-web-main/app/mailers/functional_mailer.rb
+++ marquee-web-main/app/mailers/functional_mailer.rb
@@ -1,10 +1,10 @@
 class FunctionalMailer < ActionMailer::Base
-  default :from => "MARQUEE <marquee@active.com>", :content_type => "text/html"
+  default :from => "MARQUEE <marquee@activenetwork.com>", :content_type => "text/html"
 
   def endurance_qa_effort(tdate, work_logs)
     @work_logs = work_logs
     subject = "Daily WorkLog for Endurance QA Team #{tdate.strftime('%Y/%m/%d')}"
-    mail_to = "phoenix.zhong@active.com, tyrael.tong@activenetwork.com"
+    mail_to = "phoenix.zhong@activenetwork.com, tyrael.tong@activenetwork.com"
     send_mail(mail_to, subject)
   end
 
