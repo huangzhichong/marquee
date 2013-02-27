a97e270fcff32bcb1e6b9dccb55859e2774dd6de
muyifeng1988 <muyifeng1988@gmail.com>
2013-02-27 15:54:11 +0800

    fixed verialble not found issue

--- marquee-web-main/app/controllers/home_controller.rb
+++ marquee-web-main/app/controllers/home_controller.rb
@@ -40,12 +40,13 @@ class HomeController < ApplicationController
     # @sports_overall_goal = Rails.cache.fetch("sports_overall_goal"){ 90 }
     # @membership_overall_coverage = Rails.cache.fetch("membership_overall_coverage"){ Project.caculate_coverage_by_project_and_priority("Membership","Overall") }
 
-    @regression_coverage = []
+    camps_regression_coverage = []
     %w(Overall).each do |priority|
       # @cui_coverage << Project.caculate_coverage_by_project_and_priority_and_type(project_name, priority,"CUI")
       # @aui_coverage << Project.caculate_coverage_by_project_and_priority_and_type(project_name, priority,"AUI")
-      @regression_coverage << Project.caculate_coverage_by_project_and_priority_and_type("Camps", priority,"regression")
+      camps_regression_coverage << Project.caculate_coverage_by_project_and_priority_and_type("Camps", priority,"regression")
       # @coverage << Project.caculate_coverage_by_project_and_priority(project_name,priority)
     end
+    @camps_overall_coverage = camps_regression_coverage[0]
   end
 end
