61d6ac8c23d374d03aeba1b5842e0aa1acdf28c0
Smart Huang <Smart.Huang@activenetwork.com>
2013-06-05 15:37:56 +0800

    implement api for new_build and querying test_round_staus

--- marquee-web-main/app/controllers/status_controller.rb
+++ marquee-web-main/app/controllers/status_controller.rb
@@ -1,9 +1,11 @@
 class StatusController < ApplicationController
+
   def new_build
     logger.info "New Build incoming. #{params}"
     ci_value = params[:project].split(/_|-/).map{|n| n.capitalize}.join('')
     env = params[:environment].gsub('Regression', 'QAR').gsub('INT-Latest', 'INT').gsub('P-INT', 'PINT').gsub('PQA','QA').gsub('QA-Latest','QA')
     test_environment = TestEnvironment.find_by_value(env)
+    @test_round_ids= []
     if test_environment
       test_object = "#{ci_value} #{params[:version]}"
       CiMapping.find_all_by_ci_value(ci_value).each do |ci_mapping|
@@ -11,12 +13,17 @@ class StatusController < ApplicationController
           logger.error "A CI Mapping without Browser and Operation System found: #{ci_mapping.inspect}"
           return
         end
-
         test_round = TestRound.create_for_new_build(ci_mapping.test_suite, ci_mapping.project, test_environment, User.automator, test_object, ci_mapping.browser, ci_mapping.operation_system)
         TestRoundDistributor.distribute(test_round.id)
+        @test_round_ids << test_round.id
       end
-      render :nothing => true
     end
+    render "status/new_build"
+  end
+
+  def test_round_status
+    @test_round = TestRound.find(params[:id])
+    render "status/test_round_status"
   end
 
   def update
@@ -46,37 +53,37 @@ class StatusController < ApplicationController
   end
 
   protected
-  def update_automation_script(test_round, data)
-    #    if test_round.state != "completed"
-    state = data['state'].downcase
-    automation_script_result = test_round.find_automation_script_result_by_script_name(data['script_name'])
-    if ( (state == 'done' || state == 'failed') && data['service'])
-      automation_script_result.target_services.delete_all
-      TargetService.create_services_for_automation_script_result(data['service'], automation_script_result)
+    def update_automation_script(test_round, data)
+      #    if test_round.state != "completed"
+      state = data['state'].downcase
+      automation_script_result = test_round.find_automation_script_result_by_script_name(data['script_name'])
+      if ( (state == 'done' || state == 'failed') && data['service'])
+        automation_script_result.target_services.delete_all
+        TargetService.create_services_for_automation_script_result(data['service'], automation_script_result)
+      end
+      automation_script_result.update_state!(state)
+      test_round.update_state!
+
+      if test_round.end?
+        TestRoundMailer.finish_mail(test_round.id).deliver
+      end
+      #    end
     end
-    automation_script_result.update_state!(state)
-    test_round.update_state!
 
-    if test_round.end?
-      TestRoundMailer.finish_mail(test_round.id).deliver
+    def update_automation_case(test_round, data)
+      script_name = data['script_name']
+      screen_shot_name = data["screen_shot"]
+
+      automation_script = test_round.find_automation_scirpt_by_script_name(script_name)
+      automation_script_result = test_round.find_automation_script_result_by_script_name(script_name)
+      automation_case = automation_script.find_case_by_case_id(data['case_id'])
+      automation_case_result = automation_script_result.find_case_result_by_case_id(automation_case.id)
+      automation_case_result.screen_shot = screen_shot_name
+      automation_case_result.server_log = data['server_log'] unless data['server_log'].nil?
+      automation_case_result.update!(data)
+      automation_case_result.save!
+
+      automation_script_result.update_counters_by_case_result!(automation_case_result)
+      test_round.update_counters_by_case_result!(automation_case_result)
     end
-    #    end
-  end
-
-  def update_automation_case(test_round, data)
-    script_name = data['script_name']
-    screen_shot_name = data["screen_shot"]
-
-    automation_script = test_round.find_automation_scirpt_by_script_name(script_name)
-    automation_script_result = test_round.find_automation_script_result_by_script_name(script_name)
-    automation_case = automation_script.find_case_by_case_id(data['case_id'])
-    automation_case_result = automation_script_result.find_case_result_by_case_id(automation_case.id)
-    automation_case_result.screen_shot = screen_shot_name
-    automation_case_result.server_log = data['server_log'] unless data['server_log'].nil?
-    automation_case_result.update!(data)
-    automation_case_result.save!
-
-    automation_script_result.update_counters_by_case_result!(automation_case_result)
-    test_round.update_counters_by_case_result!(automation_case_result)
-  end
 end
