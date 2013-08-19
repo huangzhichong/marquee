8505fe09a85d17d57f31a3248f4fefc116b73647
Smart Huang <Smart.Huang@activenetwork.com>
2013-08-19 16:56:37 +0800

    fix typo in notification_mailer

--- marquee-web-main/app/mailers/notification_mailer.rb
+++ marquee-web-main/app/mailers/notification_mailer.rb
@@ -1,7 +1,7 @@
 class NotificationMailer < ActionMailer::Base
   default :from => "MARQUEE <marquee@activenetwork.com>", :content_type => "text/html"
 
-    def save_to_testlink_notification(email_address,message,test_round_id,tl_project_name))    
+    def save_to_testlink_notification(email_address,message,test_round_id,tl_project_name)    
     @message  = message
     @tl_project_name = tl_project_name
     mail(to: email_address, subject: "[TestRound-#{test_round_id}] saving result to #{tl_project_name} in Testlink")
