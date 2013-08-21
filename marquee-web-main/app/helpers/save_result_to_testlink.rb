d1a0b28b34ec8623808a8a295234b6860be037ba
Smart Huang <Smart.Huang@activenetwork.com>
2013-08-21 17:09:25 +0800

    platform is not require when saveing result to testlink

--- marquee-web-main/app/helpers/save_result_to_testlink.rb
+++ marquee-web-main/app/helpers/save_result_to_testlink.rb
@@ -17,16 +17,16 @@ class TestlinkAPIClient
             "testcaseid"=>testlink_id,
             "testplanid"=>tpid,
             "buildid"=>build_id,
-            "platformid"=>platform_id,
             "status"=>tc_result,
             "notes"=>"Result from Marquee"}
+    args["platformid"]=platform_id unless platform_id == ""
     result = @server.call("tl.reportTCResult", args)
     return {:status => result[0]['status'],
             :message => result[0]['message'],
             :case_id => case_id,
             :testlink_id => testlink_id,
             :tc_result => tc_result.upcase
-          }
+            }
   end
 
   def get_test_plan_id_by_name(project_name,tp_name)
@@ -81,10 +81,12 @@ class SaveResultToTestlink
       if client.check_dev_key == true
         tp_id = client.get_test_plan_id_by_name(tl_project_name,tl_plan_name)
         @message['notes'] << "Get test plan id #{tp_id} by #{tl_project_name} and #{tl_plan_name}\n"
-        platform_id = client.get_test_platform_id_by_name(tp_id,tl_platform_name)
-        @message['notes'] << "Get platform id #{platform_id} by #{tl_plan_name} and #{tl_platform_name}\n"
         build_id = client.get_build_id_by_name(tp_id,tl_build_name)
         @message['notes'] << "Get build id #{build_id} by #{tl_plan_name} and #{tl_build_name}\n"
+        unless tl_platform_name == ""
+          platform_id = client.get_test_platform_id_by_name(tp_id,tl_platform_name)
+          @message['notes'] << "Get platform id #{platform_id} by #{tl_plan_name} and #{tl_platform_name}\n"
+        end
         test_round = TestRound.find(test_round_id)
         test_round.get_result_details.each do |result|
           if result['result'] == 'pass'
