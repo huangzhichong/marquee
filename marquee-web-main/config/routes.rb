ad9faf76454a7d6703a47c8b8155fcb44582636d
Smart Huang <Smart.Huang@activenetwork.com>
2014-04-01 14:40:18 +0800

    remove not used routes

--- marquee-web-main/config/routes.rb
+++ marquee-web-main/config/routes.rb
@@ -19,18 +19,6 @@ MarqueeWebMain::Application.routes.draw do
       resources :test_suites, :ci_mappings, :mail_notify_settings, :test_rounds, :automation_driver_configs
       get "search_automation_script", :controller => 'test_suites', :action => 'search_automation_script'
     end
-    get 'time_cards/overall', :controller => 'time_cards', :action => 'overall'
-    get 'time_cards/non_compliancy_list', :controller => 'time_cards', :action => 'non_compliancy_list'
-    get 'time_cards/reports', :controller => 'time_cards', :action => 'reports'
-    post 'time_cards/upload_time_card_report', :controller => 'time_cards', :action => 'upload_time_card_report'
-    post 'team_members/upload_team_member_list', :controller => 'team_members', :action => 'upload_team_member_list'
-    post 'time_cards/send_notification_email', :controller => 'time_cards', :action => 'send_notification_email'
-    get "dre_slide_show/configure"
-    post "dre_slide_show/update"
-    get "jira_data/effort_analysis"
-    get "jira_data/issue_status_chronicle/:key", :controller => 'jira_data', :action => 'issue_status_chronicle'
-    get "projects_display_order", :controller => "projects", :action => "display_order"
-    post "projects_display_order_update", :controller => "projects", :action => "display_order_update"
     get "activate_user/:id", :controller => "users", :action => "activate"
   end
 
@@ -89,38 +77,10 @@ MarqueeWebMain::Application.routes.draw do
     resources :automation_cases
   end
 
-  get "dre/regenerate"
-
-  get "dre/history_small"
-  get "dre/platform_dre"
-  get "dre/overall_small"
-  get "dre/overall_google_chart"
-  get "dre/automation_coverage_google_chart"
-  get "dre/in_development_open_bugs_by_priority_google_chart"
-  get "dre/overall"
-  get "dre/history_overall"
-  get "dre/history_markets"
-  get "dre/customer_impact_markets"
-  get "dre/customer_impact_other"
-  get "dre/project/:name", :controller => 'dre', :action => 'project'
-  get "dre/ext_found_bugs_by_day"
-  get "dre/custom_ext_found_bugs_by_day/:project_to_show", :controller => 'dre', :action => 'custom_ext_found_bugs_by_day'
-
   get "update_automation_script_result_triagge_result", :controller => 'automation_script_results', :action => 'update_triage_result'
   get "automation_script_results/:id/rerun", :controller => 'automation_script_results', :action => 'rerun'
   get "automation_script_results/:id/stop", :controller => 'automation_script_results', :action => 'stop'
 
-  get "dre/slide"
-  get "dre/overall_slide"
-  get "dre/history_slide"
-  get "dre/endurance_slide"
-  get "dre/camps_slide"
-  get "dre/sports_slide"
-  get "dre/nonmarket_slide"
-  get "dre/customer_impact_slide"
-  get "dre/ext_found_bugs_by_day_slide"
-
-  get "dre/platform_dre"
   get "functional/bug_report"
   get "functional/rmf_original_estimate"
   get "functional/automation_status_report"
@@ -134,30 +94,6 @@ MarqueeWebMain::Application.routes.draw do
   get "functional/endurance_qa_effort_report"
   post "functional/generate_automation_results_report"
 
-
-  get "experiment/effort_analysis_report"
-  get "experiment/ear"
-  get "experiment/effort_tracking"
-  post "experiment/effort_tracking_report"
-
-  get "dataviz/test001"
-  get "dataviz/index"
-  get "dataviz/new_in_development_tracking_config"
-  post "dataviz/create_in_development_tracking_config"
-  get "dataviz/show_report/:config_id", :controller => 'dataviz', :action => 'show_report'
-  get "dataviz/in_development_tracking_report/:config_id", :controller => 'dataviz', :action => 'in_development_tracking_report'
-  get "dataviz/pull_data_for_report/:config_id", :controller => 'dataviz', :action => 'pull_data_for_report'
-  get "dataviz/group_effort_tracking"
-
   root :to => 'home#index'
 
-  namespace :widgets do
-    get 'time_cards/show'
-    get 'time_cards/members'
-    post 'time_cards/members_select'
-  end
-  get 'data_sync/fnd_jira'
-  get 'data_sync/time_card'
-  get 'data_sync/time_card_summary'
-  get "data_sync/regenerate_dre"
 end
