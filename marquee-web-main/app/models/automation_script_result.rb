d1675f526b76bc13af66e079986a2f443a8b197b
Smart Huang <Smart.Huang@activenetwork.com>
2014-06-17 15:18:26 +0800

    can enter comments when triage test result

--- marquee-web-main/app/models/automation_script_result.rb
+++ marquee-web-main/app/models/automation_script_result.rb
@@ -77,86 +77,93 @@ class AutomationScriptResult < ActiveRecord::Base
   end
 
   def end?
-    ['done','killed','failed','timeout','not implemented','network issue'].include? self.state
-  end
+  ['done','killed','failed','timeout','not implemented','network issue'].include? self.state
+end
 
-  def not_run_cases
-    self.automation_case_results.where(:result => "not-run")
-  end
+def not_run_cases
+  self.automation_case_results.where(:result => "not-run")
+end
 
-  def failed_cases
-    self.automation_case_results.where(:result => "failed")
-  end
+def failed_cases
+  self.automation_case_results.where(:result => "failed")
+end
 
-  def update_state!(state)
-    count_automation_script_and_test_round_result(state)
-    self.state = state
-    if state == "running"
-      self.start_time = Time.now if self.start_time.blank?
-    elsif state == "done"
-      self.end_time = Time.now
-      self.not_run_cases.each do |automation_case_result|
-        automation_case_result.result = 'not-run'
-        automation_case_result.save
-      end
-      if self.not_run > 0
-        self.result = 'warning'
-      else
-        self.result = self.failed > 0 ? 'failed' : 'pass'
-      end
+def update_state!(state)
+  count_automation_script_and_test_round_result(state)
+  self.state = state
+  if state == "running"
+    self.start_time = Time.now if self.start_time.blank?
+  elsif state == "done"
+    self.end_time = Time.now
+    self.not_run_cases.each do |automation_case_result|
+      automation_case_result.result = 'not-run'
+      automation_case_result.save
+    end
+    if self.not_run > 0
+      self.result = 'warning'
     else
-      self.end_time = Time.now
-      self.result = 'failed'
+      self.result = self.failed > 0 ? 'failed' : 'pass'
     end
-    save
+  else
+    self.end_time = Time.now
+    self.result = 'failed'
   end
+  save
+end
 
-  def update_triage!(triage_result)
-    self.triage_result = triage_result
-    self.result = "failed" if triage_result == "Product Error"
-    self.result = "failed" if triage_result == "Environment Error"
-    self.result = "pass" if triage_result == "Script Error"
-    save
-  end
-
-  def duration
-    if start_time && end_time
-      end_time - start_time
-    else
-      nil
+def update_triage!(triage_result)
+  self.triage_result = triage_result
+  self.result = "failed" if triage_result.starts_with? "Product Error"
+  self.result = "failed" if triage_result.starts_with? "Environment Error"
+  if triage_result.starts_with?  "Script Error"
+    self.automation_case_results.each do |tcr|
+      tcr.result = 'pass'
+      tcr.save
     end
+    self.result = "pass"
   end
+  count_automation_script_and_test_round_result("done")
+  save
+end
 
-  def pass_count
-    self.automation_case_results.where("result='pass'").count
+def duration
+  if start_time && end_time
+    end_time - start_time
+  else
+    nil
   end
+end
 
-  def failed_count
-    self.automation_case_results.where("result='failed'").count
-  end
+def pass_count
+  self.automation_case_results.where("result='pass'").count
+end
 
-  def warning_count
-    self.automation_case_results.where("result='warning'").count
-  end
+def failed_count
+  self.automation_case_results.where("result='failed'").count
+end
 
-  def not_run_count
-    self.automation_case_results.where("result='not-run'").count
-  end
+def warning_count
+  self.automation_case_results.where("result='warning'").count
+end
 
-  def count_automation_script_result!
-    self.pass = pass_count
-    self.failed = failed_count
-    self.warning = warning_count
-    self.not_run = not_run_count
-    self.save
-  end
+def not_run_count
+  self.automation_case_results.where("result='not-run'").count
+end
 
-  def count_automation_script_and_test_round_result(state)
-    if state == 'done' || state == 'failed'
-      # update script result counting status
-      count_automation_script_result!
-      # update test round result counting status
-      self.test_round.count_test_round_result!
-    end
+def count_automation_script_result!
+  self.pass = pass_count
+  self.failed = failed_count
+  self.warning = warning_count
+  self.not_run = not_run_count
+  self.save
+end
+
+def count_automation_script_and_test_round_result(state)
+  if state == 'done' || state == 'failed'
+    # update script result counting status
+    count_automation_script_result!
+    # update test round result counting status
+    self.test_round.count_test_round_result!
   end
+end
 end
