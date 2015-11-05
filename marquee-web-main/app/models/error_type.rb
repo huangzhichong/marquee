3e7249fe610d9fb5045f7b8c48ebccd48a117b9d
Smart Huang <Smart.Huang@activenetwork.com>
2015-11-05 23:28:02 +0800

    update the display order or error type

--- marquee-web-main/app/models/error_type.rb
+++ marquee-web-main/app/models/error_type.rb
@@ -2,14 +2,14 @@ class ErrorType < ActiveRecord::Base
   has_many :automation_script_results
 
   def self.pass_and_failed_options
-    ErrorType.where("result_type in ('pass','failed')").map{|e| ["#{e.result_type.upcase} - #{e.name}", e.id]}
+    ErrorType.where("result_type in ('pass','failed')").order("result_type DESC, name ASC").map{|e| ["#{e.result_type.upcase} - #{e.name}", e.id]}
   end
 
   def self.pass_options
-    ErrorType.where("result_type in ('pass')").map{|e| ["#{e.result_type.upcase} - #{e.name}", e.id]}
+    ErrorType.where("result_type in ('pass')").order("name ASC")map{|e| ["#{e.result_type.upcase} - #{e.name}", e.id]}
   end
 
   def  self.all_options
-    ErrorType.all.map{|e| ["#{e.result_type.upcase} - #{e.name}", e.id]}
+    ErrorType.order("result_type DESC, name ASC").map{|e| ["#{e.result_type.upcase} - #{e.name}", e.id]}
   end
 end
